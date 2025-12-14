d530ec038c2966f2c23d8c197843588f79bc8197
Passos <106692628+Doctor49@users.noreply.github.com>
2025-12-13 22:28:00 -0300

    Conclusão

--- Website/scripts/script.js
+++ Website/scripts/script.js
@@ -1,12 +1,25 @@
 // Abrir/fechar o menu e empurrar o conteúdo
 const toggleBtn = document.getElementById('toggleMenu');
-const sidebar = document.getElementById('sidebarMenu');
+const sidebar = document.getElementById('sidebar');
 const content = document.getElementById('mainContent');
 
 toggleBtn.addEventListener('click', () => {
   sidebar.classList.toggle('show');
   content.classList.toggle('shift');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Produto / Serviço - Carrossel com produtos e imagens
 const carousel = document.getElementById('carouselProdutos');
@@ -32,41 +45,42 @@ carousel.addEventListener('slid.bs.carousel', function (event) {
   atualizarLista(index);
 });
 // Área para cuidar do contato
-// Máscara para telefone/celular com DDD
-const telefoneInput = document.getElementById("telefone");
-telefoneInput.addEventListener("input", function (e) {
-  let value = e.target.value.replace(/\D/g, "");
-  if (value.length > 11) value = value.slice(0, 11);
+  // Área para cuidar do contato
+  // Máscara para telefone/celular com DDD
+    const telefoneInput = document.getElementById("telefone");
+    telefoneInput.addEventListener("input", function (e) {
+      let value = e.target.value.replace(/\D/g, ""); // remove não numéricos
+      if (value.length > 11) value = value.slice(0, 11);
 
-  if (value.length > 6) {
-    e.target.value = `(${value.slice(0, 2)}) ${value.slice(2, 7)}-${value.slice(7)}`;
-  } else if (value.length > 2) {
-    e.target.value = `(${value.slice(0, 2)}) ${value.slice(2)}`;
-  } else {
-    e.target.value = value;
-  }
-});
-(function () {
-  'use strict';
-  const form = document.getElementById('form-contato');
-  form.addEventListener('submit', function (event) {
-    if (!form.checkValidity()) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    const email = document.getElementById("email").value;
-    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!regexEmail.test(email)) {
-      event.preventDefault();
-      event.stopPropagation();
-      document.getElementById("email").classList.add("is-invalid");
-    }
-    const telefone = telefoneInput.value.replace(/\D/g, "");
-    if (telefone.length !== 11) {
-      event.preventDefault();
-      event.stopPropagation();
-      telefoneInput.classList.add("is-invalid");
-    }
-    form.classList.add('was-validated');
-  }, false);
-})();
+      if (value.length > 6) {
+        e.target.value = `(${value.slice(0,2)}) ${value.slice(2,7)}-${value.slice(7)}`;
+      } else if (value.length > 2) {
+        e.target.value = `(${value.slice(0,2)}) ${value.slice(2)}`;
+      } else {
+        e.target.value = value;
+      }
+    });
+    (function () {
+      'use strict';
+      const form = document.getElementById('form-contato');
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        const email = document.getElementById("email").value;
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!regexEmail.test(email)) {
+          event.preventDefault();
+          event.stopPropagation();
+          document.getElementById("email").classList.add("is-invalid");
+        }
+        const telefone = telefoneInput.value.replace(/\D/g, "");
+        if (telefone.length !== 11) {
+          event.preventDefault();
+          event.stopPropagation();
+          telefoneInput.classList.add("is-invalid");
+        }
+        form.classList.add('was-validated');
+      }, false);
+    })();
